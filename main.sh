9665e29fdc4ccc8a103e437b05a7f9d287622f71
Allen <valternatean13@gmail.com>
2024-07-03 06:40:47 +0700

    chore: init project

---
+++ main.sh
@@ -0,0 +1,25 @@
+isInvalid=false;
+
+if [[ $# -eq 1 ]]
+then
+    if [[ "$1" == "run" ]]
+    then
+        python src/main.py;
+    elif [[ "$1" == "test" ]]
+    then
+        python -m unittest discover -s ./tests;
+    else
+        isInvalid=true;
+    fi
+else
+    isInvalid=true;
+fi
+
+if $isInvalid
+then
+    echo "usage: main.sh <command>";
+    echo "";
+    echo "Command list:";
+    echo "run - run main program";
+    echo "test - test main program against test cases";
+fi
