00c01d8f0beb511be2a0cc48b39bda264e9a3b38
Allen <valternatean13@gmail.com>
2024-07-08 18:48:47 +0700

    feat: allow raw text conversion to `TextNode`
    Note: Operator nesting is not supported

--- src/TextNode.py
+++ src/TextNode.py
@@ -1,4 +1,5 @@
 from src.LeafNode import LeafNode
+import re
 
 TEXT_NODE_TYPES = {
     "text": None,
@@ -7,6 +8,12 @@ TEXT_NODE_TYPES = {
     "code": "code",
     "link": "a",
     "image": "img"
+}
+
+OPERATORS = {
+    "**": "bold",
+    "*": "italic",
+    "`": "code"
 }
 
 class TextNode:
@@ -27,7 +34,7 @@ class TextNode:
         )
 
     def __repr__(self):
-        return f"TextNode({self.__text}, {self.__type}, {self.__url})"
+        return f"TextNode('{self.__text}', {self.__type}, {self.__url})"
 
     def to_leaf_node(self):
         supported_types = TEXT_NODE_TYPES.keys()
@@ -38,14 +45,74 @@ class TextNode:
         if not(text_type in supported_types):
             raise ValueError(f"Unknown text type. Use one of the following instead\n [{", ".join(supported_types)}]")
 
-        if type(node_url) != str:
-            raise ValueError(f"url should be a string containing a url")
-
-        if self.get_type() == "image":
+        if text_type == "image":
+            if type(node_url) != str:
+                raise ValueError(f"url should be a string containing a url")
             return LeafNode( "", TEXT_NODE_TYPES["image"], { "src": node_url })
         elif text_type == "link":
-            if (type(node_value) != str) and (len(node_value) < 1):
+            if type(node_url) != str:
+                raise ValueError(f"url should be a string containing a url")
+            elif (type(node_value) != str) and (len(node_value) < 1):
                 raise ValueError(f"anchor text should at least be a single character")
             return LeafNode( node_value, TEXT_NODE_TYPES["link"], { "href": node_url })
 
         return LeafNode(node_value, TEXT_NODE_TYPES[text_type])
+
+def split_nodes_by_delimiter(nodes, delimiter):
+    splitted_nodes = []
+    for node in nodes:
+        if node.get_type() != "text":
+            splitted_nodes.append(node)
+            continue
+
+        sections = node.get_value().split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Found unclosed delimiter")
+        
+        for (idx, section) in enumerate(sections):
+            text_type = "text" if idx % 2 == 0 else OPERATORS[delimiter]
+            if len(section) > 0:
+                splitted_nodes.append(TextNode(section, text_type))
+
+    return splitted_nodes
+            
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+    return [("image", *match) for match in matches]
+
+def extract_markdown_links(text):
+    matches = re.findall(r"[^!]?\[(.*?)\]\((.*?)\)", text)
+    return [("link", *match) for match in matches]
+
+def split_links_on_nodes(nodes):
+    splitted_nodes = []
+    for node in nodes:
+        buffer = []
+        node_text = node.get_value()
+        links = extract_markdown_images(node_text)
+        links.extend(extract_markdown_links(node_text))
+
+        for section in node_text.split(" "):
+            buffer.append(section)
+            buffer_text = " ".join(buffer)
+            for link in links:
+                link_text = (
+                    ("!" if link[0] == "image" else "")
+                    + f"[{link[1]}]({link[2]})"
+                )
+
+                found_match = ( 
+                    len(buffer_text) >= len(link_text) 
+                    and (buffer_text[-len(link_text):] == link_text)
+                )
+                
+                if found_match:
+                    splitted_nodes.append(TextNode(
+                        buffer_text[:len(buffer_text) - len(link_text)],
+                        "text"
+                    ))
+                    splitted_nodes.append(TextNode(link[1], link[0], link[2]))
+                    buffer = []
+                    break
+
+    return splitted_nodes
--- tests/test_textnode.py
+++ tests/test_textnode.py
@@ -1,5 +1,11 @@
 import unittest
-from src.TextNode import TextNode
+from src.TextNode import (
+    TextNode,
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_by_delimiter,
+    split_links_on_nodes,
+)
 
 class TestTextNode(unittest.TestCase):
     def test_equality(self):
@@ -38,6 +44,106 @@ class TestTextNode(unittest.TestCase):
             image_text.to_html(), 
             '<img src="https://i.kym-cdn.com/entries/icons/original/000/039/393/cover2.jpg">  </img>'
         )
+
+    def test_extract_images(self):
+        cases = [
+            (
+                "![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)",
+                [("image", "image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png")],
+            ),
+            (
+                "![](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)",
+                [("image", "", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png")]
+            ),
+            (
+                "![image]()",
+                [("image", "image", "")]
+            ),
+            (
+                "![](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)",
+                [
+                    ("image", "", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+                    ("image", "image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png")
+                ]
+            ),
+        ]
+
+        for case in cases:
+            extracted_link = extract_markdown_images(case[0])
+            self.assertEqual(extracted_link, case[1])
+
+    def test_extract_hyperlinks(self):
+        cases = [
+            (
+                "[link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)",
+                [("link", "link", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png")],
+            ),
+            (
+                "[](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)",
+                [("link", "", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png")]
+            ),
+            (
+                "[link]()",
+                [("link", "link", "")]
+            ),
+            (
+                "[](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)[link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)",
+                [
+                    ("link", "", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+                    ("link", "link", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png")
+                ]
+            ),
+        ]
+
+        for case in cases:
+            extracted_link = extract_markdown_links(case[0])
+            self.assertEqual(extracted_link, case[1])
+
+    def test_split_links(self):
+        cases = [
+            (
+                "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)",
+                [
+                    TextNode("This is ", "text"),
+                    TextNode("text", "bold"),
+                    TextNode(' with an ', "text", None),
+                    TextNode('italic', "italic", None),
+                    TextNode(' word and a ', "text", None),
+                    TextNode('code block', "code", None),
+                    TextNode(' and an ', "text", None),
+                    TextNode('image', "image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"),
+                    TextNode('and a ', "text", None),
+                    TextNode('link', "link", "https://boot.dev")
+                ]
+            ),
+            (
+                "![Marcianito](https://i.pinimg.com/736x/5b/49/fc/5b49fc68ece025aeb6cb8b8cd56fdf29.jpg) **ATTENTION** to *everyone* to see for `this lil fella` and [learn more here](www.google.com)",
+                [
+                    TextNode('Marcianito', "image", "https://i.pinimg.com/736x/5b/49/fc/5b49fc68ece025aeb6cb8b8cd56fdf29.jpg"),
+                    TextNode("ATTENTION", "bold"),
+                    TextNode(" to ", "text"),
+                    TextNode('everyone', "italic", None),
+                    TextNode(' to see for ', "text", None),
+                    TextNode('this lil fella', "code", None),
+                    TextNode(' and ', "text", None),
+                    TextNode('learn more here', "link", "www.google.com")
+                ]
+            ),
+        ]
+
+        for case in cases:
+            # print(f"\nTesting for: `{case[0]}`")
+            result = split_nodes_by_delimiter(
+                split_nodes_by_delimiter(
+                    split_nodes_by_delimiter(
+                        split_links_on_nodes([TextNode(case[0], "text")]), '**'
+                    ), '*'
+                ) , '`'
+            )
+
+            #print(result)
+            for idx in range(len(result)):
+                self.assertEqual(result[idx], case[1][idx])
 
 
 if __name__ == "__main__":
